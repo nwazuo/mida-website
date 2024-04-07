4ebfbc3bc8c69529b09eb8d2f4eb65c065bbdef9
Chizaram Nwazuo <chizonwazuo@yahoo.com>
2024-04-07 22:43:54 +0100

    new website data/copy usage pattern

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { InferGetStaticPropsType } from "next";
 import FadeInUp from "~/components/animation/FadeInUp";
 import SplitTextAnim from "~/components/animation/SplitTextAnim";
 import Footer from "~/components/common/Footer";
@@ -5,18 +6,20 @@ import Header from "~/components/common/Header";
 import MidaLink from "~/components/common/MidaLink";
 import GetInTouchBadge from "~/components/sections/home/GetInTouchBadge";
 import HeroVideo from "~/components/sections/home/HeroVideo";
-import homeData from "~/data/home";
 
 export default function IndexPage(
+  props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
+  const { pageData } = props
+
   return (
     <>
       <Header />
       <main className="min-h-screen">
         <div className="c-container pt-10 md:pt-16 lg:pt-24">
-          <SplitTextAnim delay={0.1} as="h2" className="max-w-[1114px] text-[#050411] text-2xl lg:text-[56px] font-semibold leading-[1.42] lg:leading-[1.42]">{homeData.title}</SplitTextAnim>
+          <SplitTextAnim delay={0.1} as="h2" className="max-w-[1114px] text-[#050411] text-2xl lg:text-[56px] font-semibold leading-[1.42] lg:leading-[1.42]">{pageData.title}</SplitTextAnim>
           <FadeInUp delay={0.3}>
-            <MidaLink href="#" className="w-fit text-xl lg:text-3xl text-black font-bold mt-6 lg:mt-10">Start a Project</MidaLink>
+            <MidaLink href="#" className="w-fit text-xl lg:text-3xl text-black font-bold mt-6 lg:mt-10">{pageData.cta}</MidaLink>
           </FadeInUp>
         </div>
         <div className="relative mb-48">
@@ -27,4 +30,17 @@ export default function IndexPage(
       <Footer />
     </>
   )
+}
+
+export function getStaticProps() {
+  const pageData = {
+    title: "Mida is a leading software development agency dedicated to delivering cutting-edge solutions.",
+    cta: "Start A Project"
+  }
+
+  return {
+    props: {
+      pageData
+    }
+  }
 }
