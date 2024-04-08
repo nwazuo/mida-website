1459184bdd59eeb9b8673d4354a2f4849277512d
ChibuezeSamObisike <samobisike@gmail.com>
2024-04-08 02:01:01 +0100

    chore: save

--- src/components/animation/FadeInUp.tsx
+++ src/components/animation/FadeInUp.tsx
@@ -1,22 +1,24 @@
 import { motion } from 'framer-motion'
-import React from 'react';
+import React from 'react'
 
 type Props = {
-  children: React.ReactNode;
-  as?: keyof JSX.IntrinsicElements;
+  children: React.ReactNode
+  as?: keyof JSX.IntrinsicElements
   /* in seconds */
-  delay?: number;
+  delay?: number
 }
 
 const FadeInUp = (props: Props) => {
   const { children, as, delay = 0 } = props
 
-  const MotionComponent = motion<React.HTMLAttributes<HTMLDivElement>>(as || 'div')
+  const MotionComponent = motion<React.HTMLAttributes<HTMLDivElement>>(
+    as || 'div',
+  )
 
   return (
     <MotionComponent
       initial={{ opacity: 0, y: 20 }}
-      whileInView={{ opacity: 1, y: 0, visibility: "visible" }}
+      whileInView={{ opacity: 1, y: 0, visibility: 'visible' }}
       viewport={{ once: true }}
       className="init-invisible"
       transition={{ delay }}
@@ -25,9 +27,5 @@ const FadeInUp = (props: Props) => {
     </MotionComponent>
   )
 }
-
-
-
-
 
 export default FadeInUp
