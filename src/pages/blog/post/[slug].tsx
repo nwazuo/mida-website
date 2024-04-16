8c29f06ac0851a2a72883167ea97dd2889c468af
Chizo Nwazuo <chizonwazuo@yahoo.com>
2024-04-16 06:27:21 +0000

    upgrade package, back button on blog

--- src/pages/blog/post/[slug].tsx
+++ src/pages/blog/post/[slug].tsx
@@ -2,10 +2,12 @@ import { chakra } from '@chakra-ui/react'
 import { PortableText } from '@portabletext/react'
 import { InferGetStaticPropsType } from 'next'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import FadeInUp from '~/components/animation/FadeInUp'
 import Footer from '~/components/common/Footer'
 import Header from '~/components/common/Header'
 import { SEO } from '~/components/seo'
+import { FaArrowLeft, FaArrowLeftLong } from 'react-icons/fa6'
 import { urlForImage } from '~/lib/sanity.image'
 
 import {
@@ -38,6 +40,7 @@ export default function Post(
   props: InferGetStaticPropsType<typeof getStaticProps>,
 ) {
   const { postData, siteSettings } = props
+  const router = useRouter()
 
   return (
     <>
@@ -45,7 +48,18 @@ export default function Post(
       <SEO title={postData.title} {...siteSettings.defaultMeta} />
       <div className="c-container pt-16 pb-16 lg:pt-36 lg:pb-20">
         <FadeInUp delay={0.1}>
-          <h1 className="text-5xl lg:text-8xl font-bold">{postData.title}</h1>
+          <button
+            onClick={router.back}
+            className="flex items-center gap-2 text-[#062041] font-bold mb-6 border border-gray-200 rounded-[4px] px-3 py-3"
+          >
+            <FaArrowLeft />
+            <span>Back to blog</span>
+          </button>
+        </FadeInUp>
+        <FadeInUp delay={0.1}>
+          <h1 className="text-5xl lg:text-8xl leading-tight lg:leading-tight font-bold">
+            {postData.title}
+          </h1>
         </FadeInUp>
         <div className="flex gap-2 lg:gap-4 mt-4 lg:mt-6">
           {postData.tags.map((t, i) => (
@@ -82,7 +96,6 @@ const portableTextComponents = {
         <div className="relative my-4">
           <Image
             src={urlForImage(value).width(1920).url()}
-            unoptimized
             alt={value.alt}
             className=""
             width={960}
