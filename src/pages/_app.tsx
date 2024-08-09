00399ef8084a91e581291dec8da7438f199ff259
ChibuezeSamObisike <samobisike@gmail.com>
2024-08-09 11:58:48 +0100

    chore: push app hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,13 @@ import '~/styles/global.css'
 
 import { ChakraProvider } from '@chakra-ui/react'
 import ReactLenis from '@studio-freight/react-lenis'
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
 import { Lato } from 'next/font/google'
-
-import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
+import Head from 'next/head'
+import Script from 'next/script' // Import Script from Next.js
 
 import theme from '~/styles/theme.chakra'
-import Head from 'next/head'
 
 export interface SharedPageProps {
   draftMode: boolean
@@ -26,7 +26,6 @@ export default function App({
   Component,
   pageProps,
 }: AppProps<SharedPageProps>) {
-  const {} = pageProps
   return (
     <>
       <style jsx global>
@@ -52,6 +51,21 @@ export default function App({
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         />
       </Head>
+
+      {/* Hotjar Tracking Code */}
+      <Script id="hotjar" strategy="afterInteractive">
+        {`
+        (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:4937444,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+      </Script>
+
       <VercelAnalytics />
       <ReactLenis root>
         <ChakraProvider theme={theme}>
