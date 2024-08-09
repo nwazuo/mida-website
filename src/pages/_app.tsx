766f10709853d3ee53db79f9c9e9339d4a7a635f
ChibuezeSamObisike <samobisike@gmail.com>
2024-08-09 12:00:12 +0100

    chore: push _app hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
 import { Lato } from 'next/font/google'
 import Head from 'next/head'
-import Script from 'next/script' // Import Script from Next.js
+import Script from 'next/script'
 
 import theme from '~/styles/theme.chakra'
 
@@ -28,6 +28,7 @@ export default function App({
 }: AppProps<SharedPageProps>) {
   return (
     <>
+      {/* Global styles for font and initial visibility */}
       <style jsx global>
         {`
           :root {
@@ -38,6 +39,8 @@ export default function App({
           }
         `}
       </style>
+
+      {/* Head section for external stylesheets */}
       <Head>
         <link
           rel="stylesheet"
@@ -55,23 +58,28 @@ export default function App({
       {/* Hotjar Tracking Code */}
       <Script id="hotjar" strategy="afterInteractive">
         {`
-        (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:4937444,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:4937444,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
         `}
       </Script>
 
+      {/* Vercel Analytics */}
       <VercelAnalytics />
+
+      {/* Main Application Components */}
       <ReactLenis root>
         <ChakraProvider theme={theme}>
           <Component {...pageProps} />
         </ChakraProvider>
       </ReactLenis>
+
+      {/* Fallback for no JavaScript */}
       <noscript>
         <style>{`.init-invisible{visibility:visible}`}</style>
       </noscript>
