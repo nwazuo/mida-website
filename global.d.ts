eb8f310d9accdf7c06ba96b08da20ea5330b7a98
ChibuezeSamObisike <samobisike@gmail.com>
2025-04-18 10:57:00 +0100

    chore: add google analytics

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,10 @@
 interface CTA {
-  text: string;
+  text: string
   link: string
+}
+
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void
+  }
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,9 @@ import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
 import { Lato } from 'next/font/google'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import Script from 'next/script'
+import { useEffect } from 'react'
 
 import theme from '~/styles/theme.chakra'
 
@@ -22,10 +24,30 @@ const primaryFontFamily = Lato({
   style: ['normal'],
 })
 
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void
+  }
+}
+
 export default function App({
   Component,
   pageProps,
 }: AppProps<SharedPageProps>) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      window.gtag?.('config', 'G-PH32G8T3WF', {
+        page_path: url,
+      })
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     <>
       {/* Global styles for font and initial visibility */}
@@ -71,6 +93,21 @@ export default function App({
             a.appendChild(r);
           })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
         `}
+      </Script>
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-PH32G8T3WF"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-PH32G8T3WF', {
+      page_path: window.location.pathname,
+    });
+  `}
       </Script>
 
       {/* Vercel Analytics */}
